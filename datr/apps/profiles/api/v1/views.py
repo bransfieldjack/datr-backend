f477e37db3df38c5ccbdf8fe21e76ca10e7d32dd
Jack Bransfield <77617959+jackbransfield@users.noreply.github.com>
2023-01-04 10:28:54 +0000

    Endpoints for user creation and token generation/retrieval.

--- datr/apps/profiles/api/v1/views.py
+++ datr/apps/profiles/api/v1/views.py
@@ -1,12 +1,15 @@
 from django.contrib.auth import get_user_model
+from django.contrib.auth.hashers import make_password
 from django.shortcuts import get_object_or_404
 from drf_yasg import openapi
 from drf_yasg.utils import swagger_auto_schema
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from rest_framework.response import Response
 
+from ...models import Token
+from ...models import User as UserModel
 from .serializers import UserSerializer
-
+from .utils import CustomizedUserPermission
 
 User = get_user_model()
 
@@ -16,6 +19,10 @@ class UserViewSet(viewsets.ModelViewSet):
     A simple ViewSet for listing or retrieving users.
     """
     http_method_names = ["post", "get", "delete"]
+
+    serializer_class = UserSerializer
+    queryset = UserModel.objects.all()
+    permission_classes = [CustomizedUserPermission, ]
 
     @swagger_auto_schema(auto_schema=None)
     def list(self, request):
@@ -35,6 +42,22 @@ class UserViewSet(viewsets.ModelViewSet):
         serializer = UserSerializer(user)
         return Response(serializer.data)
 
-    def create(self, request):
-        print("request returns: ", request)
-        return Response(request)
+    def create(self, request, *args, **kwargs):
+
+        _user = request.data['username']
+        if _user is not None:
+            # A backend authenticated the credentials
+            try:
+                user = User.objects.get(
+                    username=_user, password=make_password(request.data['password']))
+                user_token = Token.objects.get(user=user)
+                return Response({"token": str(user_token)})
+            except User.DoesNotExist:
+                user = User.objects.create(
+                    username=_user, password=make_password(request.data['password']))
+                user_token = Token.objects.get(user=user)
+                return Response({"token": str(user_token)})
+
+        else:
+            # No backend authenticated the credentials
+            return Response([], status=status.HTTP_401_UNAUTHORIZED)
